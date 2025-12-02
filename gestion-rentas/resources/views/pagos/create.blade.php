1ff4a3a12a8b893e615fb3c089f3506c56075e35
Brayan6603 <brayanarroyo778@gmail.com>
2025-12-01 18:45:59 -0600

    Fix: Mejorar estilo de los formularios

--- gestion-rentas/resources/views/pagos/create.blade.php
+++ gestion-rentas/resources/views/pagos/create.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h3>Registrar pago - {{ $propiedad->direccion ?? '' }}</h3>
+<div class="container my-4">
+    <div class="row mb-4">
+        <div class="col">
+            <h1>Registrar pago - {{ $propiedad->direccion ?? '' }}</h1>
+        </div>
+    </div>
 
-    <form action="{{ route('propiedades.pagos.store', $propiedad->id) }}" method="POST">
-        @include('pagos._form')
-    </form>
+    <div class="card">
+        <div class="card-header bg-primary-subtle">
+            <h5 class="mb-0">Registrar Nuevo Pago</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('propiedades.pagos.store', $propiedad->id) }}" method="POST" class="needs-validation" novalidate>
+                @include('pagos._form')
+            </form>
+        </div>
+    </div>
+
+    <div class="mt-4">
+        <a href="{{ route('propiedades.pagos.index', $propiedad) }}" class="btn btn-secondary">
+            <i class="fas fa-arrow-left"></i> Volver
+        </a>
+    </div>
 </div>
 @endsection
