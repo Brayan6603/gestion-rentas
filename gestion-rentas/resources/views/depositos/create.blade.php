3ffc29e743f1431d616bdf4ed9fea84e5c5ac9bf
Brayan6603 <brayanarroyo778@gmail.com>
2025-12-01 18:45:59 -0600

    Fix: Mejorar estilo de los formularios

--- gestion-rentas/resources/views/depositos/create.blade.php
+++ gestion-rentas/resources/views/depositos/create.blade.php
@@ -1,11 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h3>Registrar depósito - {{ $propiedad->direccion ?? '' }}</h3>
+<div class="container my-4">
+    <div class="row mb-4">
+        <div class="col">
+            <h1>Registrar depósito - {{ $propiedad->direccion ?? '' }}</h1>
+        </div>
+    </div>
 
-    <form action="{{ route('propiedades.depositos.store', $propiedad->id) }}" method="POST">
-        @include('depositos._form')
-    </form>
+    <div class="card">
+        <div class="card-header bg-primary-subtle">
+            <h5 class="mb-0">Registrar Nuevo Depósito</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('propiedades.depositos.store', $propiedad->id) }}" method="POST" class="needs-validation" novalidate>
+                @include('depositos._form')
+            </form>
+        </div>
+    </div>
+
+    <div class="mt-4">
+        <a href="{{ route('propiedades.depositos.index', $propiedad) }}" class="btn btn-secondary">
+            <i class="fas fa-arrow-left"></i> Volver
+        </a>
+    </div>
 </div>
 @endsection
--- gestion-rentas/resources/views/pagos/edit.blade.php
+++ gestion-rentas/resources/views/pagos/edit.blade.php
@@ -1,12 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h3>Editar pago #{{ $pago->id }} - {{ $propiedad->direccion ?? '' }}</h3>
+<div class="container my-4">
+    <div class="row mb-4">
+        <div class="col">
+            <h1>Editar pago #{{ $pago->id }} - {{ $propiedad->direccion ?? '' }}</h1>
+        </div>
+    </div>
 
-    <form action="{{ route('propiedades.pagos.update', [$propiedad->id, $pago->id]) }}" method="POST">
-        @method('PUT')
-        @include('pagos._form')
-    </form>
+    <div class="card">
+        <div class="card-header bg-primary-subtle">
+            <h5 class="mb-0">Editar Pago</h5>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('propiedades.pagos.update', [$propiedad->id, $pago->id]) }}" method="POST" class="needs-validation" novalidate>
+                @method('PUT')
+                @include('pagos._form')
+            </form>
+        </div>
+    </div>
+
+    <div class="mt-4">
+        <a href="{{ route('propiedades.pagos.show', [$propiedad, $pago]) }}" class="btn btn-secondary">
+            <i class="fas fa-arrow-left"></i> Volver
+        </a>
+    </div>
 </div>
 @endsection
